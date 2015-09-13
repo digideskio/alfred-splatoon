323f58e03cf10428304b951e0e1d1437d3b1c319
Phillip Lanclos <flipxfx@gmail.com>
2015-09-13 18:32:49 -0500

    Remove uneeded scheduleNames, testing update

--- src/splatoon.py
+++ src/splatoon.py
@@ -11,20 +11,18 @@ ICON_RANKED = "./ranked.png"
 def main(wf):
     # Get current maps
     def get_maps():
-        url = "http://splatoon.ink/schedule.json"
-        scheduleNames = ["Current", "Next", "Later"]
-
         try:
-            response = web.get(url)
+            # Get schedule
+            response = web.get("http://splatoon.ink/schedule.json")
             if response.status_code == 200:
-                # Get schedule and ensure it's ordered correctly
+                # Ensure schedule is ordered correctly
                 schedule = sorted(response.json()["schedule"], key=lambda session: session["startTime"])
 
                 # Add map rotations
                 for index, session in enumerate(schedule):
-                    schedulePrefix = "Current" if index == 0 else datetime.datetime.fromtimestamp(session["startTime"] / 1000).strftime("%I %p").lstrip("0")
-                    if session["regular"]: wf.add_item(reduce(lambda m1, m2: m1["nameEN"] + ", " + m2["nameEN"], session["regular"]["maps"]), schedulePrefix + " Turf Maps", valid=False, icon=ICON_REGULAR)
-                    if session["ranked"]: wf.add_item(reduce(lambda m1, m2: m1["nameEN"] + ", " + m2["nameEN"], session["ranked"]["maps"]), schedulePrefix + " " + session["ranked"]["rulesEN"] + " Maps", valid=False, icon=ICON_RANKED)
+                    sessionPrefix = "Current" if index == 0 else datetime.datetime.fromtimestamp(session["startTime"] / 1000).strftime("%I %p").lstrip("0")
+                    if session["regular"]: wf.add_item(reduce(lambda m1, m2: m1["nameEN"] + ", " + m2["nameEN"], session["regular"]["maps"]), sessionPrefix + " Turf Maps", valid=False, icon=ICON_REGULAR)
+                    if session["ranked"]: wf.add_item(reduce(lambda m1, m2: m1["nameEN"] + ", " + m2["nameEN"], session["ranked"]["maps"]), sessionPrefix + " " + session["ranked"]["rulesEN"] + " Maps", valid=False, icon=ICON_RANKED)
             else:
                 wf.add_item("Error getting maps", "Unable to retrieve maps", valid=False, icon=ICON_ERROR)
         except:
